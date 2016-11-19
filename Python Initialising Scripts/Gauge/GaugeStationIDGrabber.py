9b4a957c10f7c7af942c9d9f1ce85646c1a6833e
James M <jm824@kent.ac.uk>
2016-11-19 23:45:15 +0000

    Updated StationIDGrabber
    Improved StationIDGrabber script and added test to check if the returned
    list contains expected station ids

--- Python Initialising Scripts/Gauge/GaugeStationIDGrabber.py
+++ Python Initialising Scripts/Gauge/GaugeStationIDGrabber.py
@@ -2,20 +2,28 @@ import simplejson
 import urllib3
 
 """
-One time script to grab all the station references (unique id's) from the EA API and save them in a file
+One time script to grab all the station references (unique id's) from the EA API and save them in
+a text file. This us useful for testing and saving making too many HTTP requests
 """
-
 http = urllib3.PoolManager()
-stationReq = http.request('GET','http://environment.data.gov.uk/flood-monitoring/id/stations')
+stationReq = http.request('GET', 'http://environment.data.gov.uk/flood-monitoring/id/stations')
 json = simplejson.loads(stationReq.data.decode('utf-8'))
 json.get('items')
 
-file = open('data/newGaugeStationsIDs.txt','w')
-for i in json.get('items'):
-    file.write(i.get('@id'))
-    file.write('\n')
-file.close()
+
+def save_station_ids():
+    file = open('data/newGaugeStationsIDs.txt', 'w')
+    for i in json.get('items'):
+        file.write(i.get('@id'))
+        file.write('\n')
+    file.close()
 
 
+# Check to see if an id is in the list returned by the API
+def check_for_id(id):
+    for i in json.get('items'):
+        if i.get('@id') == 'http://environment.data.gov.uk/flood-monitoring/id/stations/' + id:
+            return True
+    return False
 
-
+save_station_ids()
