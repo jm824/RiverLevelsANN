6e8608c7db5d1c362e3c7b05b8d639d2699a899e
James M <jm824@kent.ac.uk>
2016-11-19 22:44:54 +0000

    Updated stationGrabber
    Updated station grabber to use urllib3. Also removed redundant code

--- Python Initialising Scripts/Gauge/stationGrabber.py
+++ Python Initialising Scripts/Gauge/stationGrabber.py
@@ -1,71 +1,61 @@
 import simplejson
 import urllib3
-import urllib.request
+
 
 def printStationDetails(stationId):
-	http = urllib3.PoolManager()
-	req = http.request('GET','http://environment.data.gov.uk/flood-monitoring/id/stations/' + stationId)
-	opener = urllib3.build_opener()
+    req2 = http.request('GET', 'http://environment.data.gov.uk/flood-monitoring/id/stations')
+    json = simplejson.loads(req2.data.decode('utf-8'))
 
-	f = opener.open(req)
-	json = simplejson.load(f)
-
-	print('--------------------------------------')
-	print('Station ID = ' + json.get('items').get('stationReference'))
-	print(json.get('items').get('catchmentName'))
-	print(json.get('items').get('eaRegionName'))
-	print(json.get('items').get('eaAreaName'))
-	print(json.get('items').get('label'))
-	print(json.get('items').get('riverName'))
-	print(json.get('items').get('town'))
-	print(json.get('items').get('lat'))
-	print(json.get('items').get('long'))
-	print(json.get('items').get('easting'))
-	print(json.get('items').get('northing'))
-	try:
-		print(json.get('items').get('stageScale').get('datum'))
-		print(json.get('items').get('stageScale').get('highestRecent').get('dateTime'))
-		print(json.get('items').get('stageScale').get('highestRecent').get('value'))
-		print(json.get('items').get('stageScale').get('maxOnRecord').get('dateTime'))
-		print(json.get('items').get('stageScale').get('maxOnRecord').get('value'))
-		print(json.get('items').get('stageScale').get('minOnRecord').get('dateTime'))
-		print(json.get('items').get('stageScale').get('minOnRecord').get('value'))
-		print(json.get('items').get('stageScale').get('scaleMax'))
-		print(json.get('items').get('stageScale').get('typicalRangeHigh'))
-		print(json.get('items').get('stageScale').get('typicalRangeLow'))
-	except AttributeError:
-		print('NO STAGESCALE DATA')
-	print(json.get('items').get('dateOpened'))
-	print(json.get('items').get('RLOIid'))
-	print(json.get('items').get('wiskiID'))
-	print('--------------------------------------')
+    for i in json:
+        print('--------------------------------------')
+        print('Station ID = ' + i.get('items').get('stationReference'))
+        print(i.get('items').get('catchmentName'))
+        print(i.get('items').get('eaRegionName'))
+        print(i.get('items').get('eaAreaName'))
+        print(i.get('items').get('label'))
+        print(i.get('items').get('riverName'))
+        print(i.get('items').get('town'))
+        print(i.get('items').get('lat'))
+        print(i.get('items').get('long'))
+        print(i.get('items').get('easting'))
+        print(i.get('items').get('northing'))
+        try:
+            print(i.get('items').get('stageScale').get('datum'))
+            print(i.get('items').get('stageScale').get('highestRecent').get('dateTime'))
+            print(i.get('items').get('stageScale').get('highestRecent').get('value'))
+            print(i.get('items').get('stageScale').get('maxOnRecord').get('dateTime'))
+            print(i.get('items').get('stageScale').get('maxOnRecord').get('value'))
+            print(i.get('items').get('stageScale').get('minOnRecord').get('dateTime'))
+            print(i.get('items').get('stageScale').get('minOnRecord').get('value'))
+            print(i.get('items').get('stageScale').get('scaleMax'))
+            print(i.get('items').get('stageScale').get('typicalRangeHigh'))
+            print(i.get('items').get('stageScale').get('typicalRangeLow'))
+        except AttributeError:
+            print('NO STAGESCALE DATA')
+        print(i.get('items').get('dateOpened'))
+        print(i.get('items').get('RLOIid'))
+        print(i.get('items').get('wiskiID'))
+        print('--------------------------------------')
 
 
-req = urllib3.Request('http://environment.data.gov.uk/flood-monitoring/id/stations?_limit=10000')
-opener = urllib3.build_opener()
-f = opener.open(req)
-json = simplejson.load(f)
-
+http = urllib3.PoolManager()
+req = http.request('GET', 'http://environment.data.gov.uk/flood-monitoring/id/stations')
+json = simplejson.loads(req.data.decode('utf-8'))
 items = json.get('items')
 
 count = 0
 theError = 'empty'
 errorCount = 0
 for i in items:
-	count = count + 1
-	
-	if count < -1:
-		print('nope')
-
-	else:
-		try:
-			printStationDetails(i.get('notation'))
-		except:
-			continue
+    count = count + 1
+    if count < -1:
+        print('nope')
+    else:
+        try:
+            printStationDetails(i.get('notation'))
+        except:
+            continue
 
 print(count)
 print(theError)
 print(errorCount)
-
-
-
