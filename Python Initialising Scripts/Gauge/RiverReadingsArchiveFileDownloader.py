b787b021202595c2c227fc4519d636661ed11fae
James M <jm824@kent.ac.uk>
2016-11-23 17:53:23 +0000

    Updated archive downloader script
    Former-commit-id: b346de908836c63f1a338f7a463850cc1743465e

--- Python Initialising Scripts/Gauge/RiverReadingsArchiveFileDownloader.py
+++ Python Initialising Scripts/Gauge/RiverReadingsArchiveFileDownloader.py
@@ -2,16 +2,20 @@ import urllib3
 import shutil
 import datetime
 
+"""
+One time script to download all 15min archived river level readings from the EA online archive (API).
+Simply specify the start and end date.
+"""
 
 http = urllib3.PoolManager()
-prefix = 'riverLevelArchive_'
+prefix = 'riverLevelArchive_' #The prefix chosen for files names
 url = 'http://environment.data.gov.uk/flood-monitoring/archive?date='
-date = datetime.date(2016,11,27)
-while(date.strftime('%Y-%m-%d') != '2017-01-21'):
-
-    currentDate = date.strftime('%Y-%m-%d')
-
-    print(date.strftime('%Y-%m-%d'))
+startDate = datetime.date(2016, 11, 17)
+endDate = datetime.date(2016, 11, 18)
+while startDate != endDate:
+    currentDate = startDate.strftime('%Y-%m-%d')
+    print(startDate.strftime('%Y-%m-%d'))
     with http.request('GET', url + currentDate, preload_content=False) as r, open('data/RiverGauges/archivedRiverLevelRecordings/' + prefix + currentDate + '.csv', 'wb') as out_file:
         shutil.copyfileobj(r, out_file)
-    date = date + datetime.timedelta(1)
+    #+1 day
+    startDate = startDate + datetime.timedelta(1)
