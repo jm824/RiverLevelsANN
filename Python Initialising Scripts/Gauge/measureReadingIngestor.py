cb263a5f31c92253e0e797784b9ac66e54ad2e75
James M <jm824@kent.ac.uk>
2016-11-29 20:50:39 +0000

    Improved readings ingestor script
    Updated the readings ingestor script to take arguments when called from
    the command line
    
    
    Former-commit-id: a9350011e7c7c7522f477e02fd04fab1f1d8cb74

--- Python Initialising Scripts/Gauge/measureReadingIngestor.py
+++ Python Initialising Scripts/Gauge/measureReadingIngestor.py
@@ -2,61 +2,77 @@ import datetime
 import psycopg2
 import csv
 
-connection = 'dbname=trout user=postgres password=67a256 host=localhost port=5432'
-try:
-    dbconn = psycopg2.connect(connection)
-    cur = dbconn.cursor()
-except:
-    print('Connection to the database could not be established')
+"""
+Script to read in data from the archived river level recordings csv files.
+Specify the start and end dates (inclusive)
+Optional - specify the directory containing the archive files
+"""
 
-#starting date to read from
-startDate = datetime.date(2016, 11, 1)
+def read_in_archive_data(startDate, endDate, path):
+    connection = 'dbname=trout user=postgres password=67a256 host=localhost port=5432'
+    try:
+        dbconn = psycopg2.connect(connection)
+        cur = dbconn.cursor()
+    except:
+        print('Connection to the database could not be established')
 
-#for each day/file
-while(startDate.strftime('%Y-%m-%d') != '2017-01-19'):
-    currentDate = startDate.strftime('%Y-%m-%d')
+    #for each day/file
+    while(startDate != endDate):
+        currentDate = startDate.strftime('%Y-%m-%d')
 
-    countEntered = 0
-    countIgnored = 0
-    with open('data/riverGauges/archivedRiverLevelRecordings/riverLevelArchive_' + currentDate +'.csv', 'r') as csvFile:
-        data = csv.reader(csvFile, delimiter=',')
-        reading = {}
-        for row in data:
-            if row[0] == 'dateTime': continue #skip heading row
-            time = row[0].split(':',1)
-            try:
-                if time[1] != '00:00Z': continue #skip readings not on the hour
-            except IndexError:
-                print(row)
-                exit()
-            if row[1].split('/')[-1] != 'E47061-level-stage-i-15_min-mASD': continue
-            reading['dateTime'] = row[0]
-            reading['measureId'] = row[1].split('/')[-1]
-            reading['value'] = row[2]
+        countEntered = 0
+        countIgnored = 0
+        if path == None:
+            path = ''
+        with open(path + 'riverLevelArchive_' + currentDate +'.csv', 'r') as csvFile:
+            data = csv.reader(csvFile, delimiter=',')
+            reading = {}
+            for row in data:
+                if row[0] == 'dateTime': continue #skip heading row
+                reading['dateTime'] = row[0]
+                reading['measureId'] = row[1].split('/')[-1]
+                reading['value'] = row[2]
 
-            try:
-                readingSQL = "INSERT INTO HourlyGaugeReading" \
-                             "(dateTime, measureId, value)" \
-                             "VALUES (%(dateTime)s, %(measureId)s, %(value)s);"
-                cur.execute(readingSQL, reading)
-                countEntered +=1
-                dbconn.commit()
-            except psycopg2.DataError:
-                #Reach here if the format of the reading is not correct
-                #print('DATA ERROR: ' + reading['measureId'])
-                countIgnored +=1
-                dbconn.rollback()
-            except psycopg2.IntegrityError:
-                #Reach here if either the reading does not have a station or if it is already entered
-                #print(reading['measureId'])
-                countIgnored += 1
-                dbconn.rollback()
+                try:
+                    readingSQL = "INSERT INTO GaugeReading" \
+                                 "(dateTime, measureId, value)" \
+                                 "VALUES (%(dateTime)s, %(measureId)s, %(value)s);"
+                    cur.execute(readingSQL, reading)
+                    countEntered +=1
+                    dbconn.commit()
+                except psycopg2.DataError:
+                    #Reach here if the format of the reading is not correct
+                    countIgnored +=1
+                    dbconn.rollback()
+                except psycopg2.IntegrityError:
+                    #Reach here if either the reading does not have a station or if it is already entered
+                    countIgnored += 1
+                    dbconn.rollback()
 
-            #print(countEntered)
-            #print(countIgnored)
-            #print('==========')
+        print('Number of entries: ' + countEntered)
+        print('Number ignored: ' + countIgnored)
+        print('Reached end of file for ' + currentDate)
+        startDate = startDate + datetime.timedelta(1)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='argument handler')
+    parser.add_argument('-start', '-s', required= True, help='Start date in dd-mm-yyyy format')
+    parser.add_argument('-end', '-e', required=True, help='End date in dd-mm-yyyy format')
+    parser.add_argument('-path', '-p', required=False, help='The directory containing the data file ')
+    args = parser.parse_args()
+
+    try:
+        startDate = datetime.datetime.strptime(args.start, '%d-%m-%Y')
+        endDate = datetime.datetime.strptime(args.end, '%d-%m-%Y')
+    except ValueError:
+        print('The dates were not entered in the correct format of dd-mm-yyyy')
+        exit()
+
+    try:
+        read_in_archive_data(startDate, endDate, args.path)
+    except FileNotFoundError:
+        print('No such file or directory')
 
 
 
-    print('end of file')
-    startDate = startDate + datetime.timedelta(1)
