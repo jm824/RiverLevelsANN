ce520878f69db5d207d1f37e10b7542a666334fc
James M <jm824@kent.ac.uk>
2016-12-07 13:29:44 +0000

    Updated catchmentsKMLWriter script
    Updated the script to use more simple code
    
    
    Former-commit-id: b06f41edd504f704b57ad34825d87989220e9706

--- Python Initialising Scripts/KML/catchmentsKMLWriter.py
+++ Python Initialising Scripts/KML/catchmentsKMLWriter.py
@@ -2,30 +2,21 @@ import os
 from xml.etree import ElementTree
 from bngToLatLong import OSGB36toWGS84
 
+#Read in the polygon catchment data
 file_name = 'WFD_River_Waterbody_Catchments_Cycle2.gml'
 full_file = os.path.abspath(os.path.join('data', file_name))
+tree = ElementTree.parse(full_file)
+polygon = tree.findall('featureMember/WFD_River_Waterbody_Catchments/polygonProperty/Polygon/outerBoundaryIs/LinearRing/coordinates')
 
-dom = ElementTree.parse(full_file)
-#coords = dom.findall('boundedBy/Box/coordinates')
-polygon = dom.findall('featureMember/WFD_River_Waterbody_Catchments/multiPolygonProperty/MultiPolygon/polygonMember/Polygon/outerBoundaryIs/LinearRing/coordinates')
-
-count = 0
-insideCount = 0
-for p in polygon: #for each polygon
+for p in polygon:  # for each polygon
     polyString = ""
     coordSet = p.text.split()
-    count+=1
-    for c in coordSet: #for each set of polygon points
+    for c in coordSet:  # for each set of polygon points, convert them
         coords = c.split(',')
-        coords = [float(i) for i in coords] #change to type float
-        returnedVal = OSGB36toWGS84(coords[0],coords[1])
-        insideCount+=1
-        polyString += str(returnedVal[0]) + "," + str(returnedVal[1]) + " "
+        coords = [float(i) for i in coords]  # change to type float
+        returnedVal = OSGB36toWGS84(coords[0], coords[1])
+        polyString += str(returnedVal[1]) + "," + str(returnedVal[0]) + " "
+    #edit the xml to include the new polygon in latlong
     p.text = polyString
 
-dom.write('glm2.xml')
-
-#print(count)
-#print(insideCount)
-
-
+tree.write('WFD_River_Waterbody_Catchments_Cycle2_in_latlong.kml')
