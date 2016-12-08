2530b6be8bb8d385b131b17ab82fd864e6cddac9
James M <jm824@kent.ac.uk>
2016-12-08 12:50:44 +0000

    Simplified catchmentsKMLWriter.py
    Simplified polygon to kml script to use simplekml
    
    
    Former-commit-id: 2045c08cafdf5e373e08f0d781bef9ea83aa6c7b

--- Python Initialising Scripts/KML/catchmentsKMLWriter.py
+++ Python Initialising Scripts/KML/catchmentsKMLWriter.py
@@ -1,22 +1,24 @@
 import os
 from xml.etree import ElementTree
 from bngToLatLong import OSGB36toWGS84
+import simplekml
 
 #Read in the polygon catchment data
 file_name = 'WFD_River_Waterbody_Catchments_Cycle2.gml'
 full_file = os.path.abspath(os.path.join('data', file_name))
 tree = ElementTree.parse(full_file)
 polygon = tree.findall('featureMember/WFD_River_Waterbody_Catchments/polygonProperty/Polygon/outerBoundaryIs/LinearRing/coordinates')
-
-for p in polygon:  # for each polygon
-    polyString = ""
+kml = simplekml.Kml()
+for p in polygon:  #for each polygon
+    polygon = []
     coordSet = p.text.split()
-    for c in coordSet:  # for each set of polygon points, convert them
+    for c in coordSet:  #for each set of polygon points, convert them
         coords = c.split(',')
         coords = [float(i) for i in coords]  # change to type float
         returnedVal = OSGB36toWGS84(coords[0], coords[1])
-        polyString += str(returnedVal[1]) + "," + str(returnedVal[0]) + " "
-    #edit the xml to include the new polygon in latlong
-    p.text = polyString
+        polygon.append(returnedVal)
+    #add translated polygon to new kml
+    pol = kml.newpolygon(name='Polygon')
+    pol.outerboundaryis = polygon
 
-tree.write('WFD_River_Waterbody_Catchments_Cycle2_in_latlong.kml')
+kml.save('simpleCatchments.kml')
