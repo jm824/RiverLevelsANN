cf4ba79b2f1954c35e1e446849d6ebb41d828f23
James M <jm824@kent.ac.uk>
2016-12-13 10:09:11 +0000

    Comment change to srew ingestor script
    Former-commit-id: 15703624b13417fae3299b92bada6bd29fd870a3

--- Python Initialising Scripts/SREW/ValidSrewLatLongToDB.py
+++ Python Initialising Scripts/SREW/ValidSrewLatLongToDB.py
@@ -5,6 +5,7 @@ import psycopg2
 This is a one time run script
 Enter all SREW stations into database for which we have a lat long for
 In other words enter all valid SREW stations into the local database
+Stations are read from a static file.
 """
 
 #Open csv file that contains the ids for all the valid SREW stations
