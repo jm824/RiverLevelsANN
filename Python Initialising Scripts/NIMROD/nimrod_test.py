f03e987b83226d7806ef3fcfbbd9a9f9b601d9d0
James M <jm824@kent.ac.uk>
2017-01-26 18:01:36 +0000

    Reduced nimrod reader to opne python script
    The nimrod bounding box script is now for python 3. Reduced nimrod work
    to one script which now produced KML to plot the rain radar bounding box
    
    
    Former-commit-id: 4a67383410d20206f93776cd9aacb5e4b6744998

--- Python Initialising Scripts/NIMROD/nimrod_test.py
+++
@@ -1,8 +0,0 @@
-import nimrod
-a = nimrod.Nimrod(open(
-    'metoffice-c-band-rain-radar_uk_201601010000_1km-composite.dat.gz'))
-a.query()
-a.extract_asc(open('full_raster.asc', 'w'))
-a.apply_bbox(279906, 285444, 283130, 290440)
-a.query()
-a.extract_asc(open('clipped_raster.asc', 'w'))
