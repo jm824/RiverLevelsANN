085e5bbe1ab6a563000d5050913e6e6699ca70e7
James M <jm824@kent.ac.uk>
2017-02-22 00:09:04 +0000

    Added additional script to enter in records with null data where in the database where there are missing records for gauge measures. This allows for easy querying of missing values and these can easily be removed later.

---
+++ Python Initialising Scripts/Gauge/gaugemeasure_fill_blanks.py
@@ -0,0 +1,54 @@
+import nimrod_reader
+import datetime
+import psycopg2
+
+"""
+This script reads through the database and creates an entry for readings that are missing, setting
+the value to NULL
+"""
+
+class ReadInNimrod:
+    def __init__(self, startdatetime, enddatetime, measure):
+        self.station = measure
+        try:
+            self.startdatetime = datetime.datetime.strptime(startdatetime, '%d-%m-%Y %H:%M')
+            self.enddatetime = datetime.datetime.strptime(enddatetime, '%d-%m-%Y %H:%M')
+        except:
+            exit('One or more provided date was not in the correct format or dd-mm-yyyy hh-mm')
+
+        # Try to establish handle to the database
+        connection = 'dbname=trout user=postgres password=67a256 host=localhost port=5432'
+        try:
+            self.dbconn = psycopg2.connect(connection)
+            self.cur = self.dbconn.cursor()
+        except:
+            print('Connection to the database could not be established')
+
+        self.fill_in_blanks()
+
+    def fill_in_blanks(self):
+        while self.startdatetime.time() <= self.enddatetime.time() and self.startdatetime.date() <= self.enddatetime.date():
+            self.cur.execute("SELECT value FROM hourlygaugereading WHERE measureid = (%s) AND datetime = (%s)",(self.station,self.startdatetime,))
+            result = self.cur.fetchall()
+            if not result:
+                reading = {}
+                reading['id'] = self.station
+                reading['datetime'] = self.startdatetime
+                reading['value'] = None
+                try:
+                    sql = "INSERT INTO hourlygaugereading" \
+                          "(datetime, measureid, value)" \
+                          "VALUES (%(datetime)s, %(id)s, %(value)s);"
+                    self.cur.execute(sql, reading)
+                    self.dbconn.commit()
+                except psycopg2.IntegrityError:
+                    self.dbconn.rollback()
+
+            self.startdatetime += datetime.timedelta(minutes=15)
+
+
+obj = ReadInNimrod('01-01-2017 00:00', '31-01-2017 23:45', '2133-level-stage-i-15_min-mASD')
+
+
+
+
