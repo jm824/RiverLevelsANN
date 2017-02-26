790857952cf08e66d8bca01b95e1adfc8762d680
James M <jm824@kent.ac.uk>
2017-02-26 00:52:01 +0000

    Updated dataset builder script but still a work in progress with command line functionality still not working. Using hard coding this can now build training sets to specification

--- Python Initialising Scripts/NeuralNet/dataset_builder.py
+++ Python Initialising Scripts/NeuralNet/dataset_builder.py
@@ -16,14 +16,18 @@ gives us better control over the training data.
 """
 
 class DatasetBuilder:
-    def __init__(self, starttime, endtime, catchmenfile, outputfile):
-        self.catchmentDetails = catchmenfile
-        self.startTime = datetime.datetime.strptime(starttime, '%d-%m-%Y-%H%M')
-        self.endTime = datetime.datetime.strptime(endtime,'%d-%m-%Y-%H%M')
-        self.read_config()
-        self.gaugemeasure = None
+    def __init__(self, starttime, endtime, measure, outputfile, outputhour):
+        try:
+            self.startTime = datetime.datetime.strptime(starttime, '%d-%m-%Y-%H%M')
+            self.endTime = datetime.datetime.strptime(endtime,'%d-%m-%Y-%H%M')
+        except:
+            exit("Start time and/or end time was provided in an incorrect format. Correct format is dd-mm-yyyy-hhmm")
+
+        self.gaugemeasure = measure
+        self.epoch = outputhour
         self.srewstation = None
         self.catchment = None
+        self.nimrodcells = None
         self.inputrow = []
 
         #Try to establish handle to the database
@@ -34,38 +38,42 @@ class DatasetBuilder:
         except:
             print('Connection to the database could not be established')
 
-        self.read_config()
-        with open(outputfile, 'w', newline='') as csvfile:
+        self.read_config() #Read in all the required reference data about the catchment
+        with open(outputfile, 'w', newline='') as csvfile: #Open a file to write output to
             writer = csv.writer(csvfile, delimiter=',')
 
             count = 0
+            #Here we are reading in hour by hour
             while self.startTime < self.endTime:
-                pastriver = self.ingest_past_measure_readings(self.startTime)
-                if pastriver:
+                pastgauge = self.ingest_past_measure_readings(self.startTime)
+                if pastgauge:
                     srew = self.ingest_srew(self.startTime)
-                    rain = self.ingest_nimrod(self.startTime)
-                    futureriver = self.ingest_future_measure_readings(self.startTime)
-                if srew and rain and futureriver:
-                    self.inputrow = pastriver + srew + rain + futureriver
-                    count += 1
-                    writer.writerow(self.inputrow)
+                    nimrod = self.ingest_nimrod(self.startTime)
+                    futuregauge = self.ingest_future_measure_readings(self.startTime)
+                    if srew and futuregauge and nimrod: #If we got all the required data from the db for this epoch
+                        self.inputrow = pastgauge + srew + nimrod + futuregauge
+                        count += 1
+                        writer.writerow(self.inputrow)
                 self.startTime += datetime.timedelta(hours=1)
-                print(count)
+                #print(count)
 
             print(count)
+        csvfile.close()
 
-    #read in the csv file which contains info about the catchment
+    #read in the catchment details from the database using the catchment's measure as reference
     def read_config(self):
-        with open('data/' + self.catchmentDetails) as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=',')
-            row = next(csvreader)
-            self.gaugemeasure = row[0]
-            self.srewstation = row[1]
-            self.catchment = row[2]
+        self.cur.execute("SELECT id, raincells, srewstation FROM catchments WHERE measure = %s", (self.gaugemeasure,))
+        result = self.cur.fetchall()
+        if result:
+            self.catchment = result[0][0]
+            self.nimrodcells = result[0][1]
+            self.srewstation = result[0][2]
+        else:
+            exit("No record found for the given river measure.")
 
     def ingest_nimrod(self, start):
         values = []
-        bottomtimewindow = start - datetime.timedelta(hours=336)
+        bottomtimewindow = start - datetime.timedelta(hours=12)
 
         self.cur.execute("SELECT data FROM nimrodreading " \
                          "WHERE id = %s" \
@@ -77,41 +85,33 @@ class DatasetBuilder:
 
         result = self.cur.fetchall()
 
-        bounds = [0,12,24,48,60,72,144,288,432,786,2016,4032] #intervals at which to collate readings
-        #1,2,3,4,5,6,6-12,12-24,24-36,36-48,48-1week,1week-2week
+        bounds2 = [0,12,24,48,60,72,144,288,432,786,2016,4032] #intervals at which to collate readings
+        bounds = [0,12,24,36,48,60,72,84,96,108,120]
 
-        #check data for missing periods within the first 5 hours
-        missing = 0
-        for r in result[0:72]:  # check how many missing 5 min periods we have in the first 5 hours
-            if not r[0]:
-                missing += 1
-            if missing >= 12:
-                values = None
-                return values
-        # check data for missing periods the rest of the two week period allowing for 16 hours of missing data
-        missing = 0
-        for r in result[72:4032]:
-            if not r[0]:
-                missing += 1
-            if missing >= 200:
-                values = None
-                return values
-
-        #collate and load data into list
-        for i in range(len(bounds)-1):
-
-            total = 0
-            for tuple in result[bounds[i]:bounds[i + 1]]: #for each input node
-                if tuple[0]:
-                    for value in tuple[0]:
-                        total += value/32
-            values.append(total)
+        values = []
+        # for each bound -1
+        for i in range(len(bounds) - 1):
+            missing = 0
+            hourlyprecip = 0
+            for scan in result[bounds[i]:bounds[i + 1]]:  #for each scan of each tuple pair making up an hour
+                if not scan[0]: #if 5min scan missing
+                    missing += 1
+                    if missing >= 2:  #if there are too many missing readings then abort, allow 2*5min missing for each hour
+                        values = None
+                        return values
+                else: #else total up each 5 min period for the hour
+                    scanavg = 0
+                    for cell in scan[0]:
+                        if cell < 0: cell = 0
+                        scanavg += cell
+                    hourlyprecip += (scanavg/32)/len(scan[0])
+            values.append(hourlyprecip)
 
         return values
 
     def ingest_srew(self, start):
         values = []
-        bottomtimewindow = start - datetime.timedelta(hours=336)
+        bottomtimewindow = start - datetime.timedelta(hours=720)
 
         self.cur.execute("SELECT value FROM hourlysrewreading " \
             "WHERE stationid = %s" \
@@ -121,7 +121,7 @@ class DatasetBuilder:
 
         result = self.cur.fetchall()
 
-        bounds = [0, 1, 2, 3, 4, 5, 12, 24, 36, 48, 168, 336]  # intervals at which to collate readings
+        bounds = [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12, 24, 36, 48, 168, 336, 720]  #intervals at which to collate readings
 
         for i in range(len(bounds)-1):
             total = 0
@@ -135,7 +135,7 @@ class DatasetBuilder:
         bottomtimewindow = start - datetime.timedelta(hours=6)
         values = []
 
-        #Get the hourly readings going back 6 hours from timenow (not including timenow readings)
+        #Get the hourly readings going back 6 hours from start (not including start readings which is considered 1 hour into the future)
         self.cur.execute("SELECT value, datetime FROM hourlygaugereading " \
                          "WHERE measureid = %s" \
                             "AND extract('minutes' from datetime) = 0" \
@@ -146,7 +146,7 @@ class DatasetBuilder:
                          datetime.datetime.strftime(bottomtimewindow, '%d-%m-%Y %H:%M'),))
 
         result = self.cur.fetchall()
-        for r in result:
+        for r in result: #Check for missing readings and attempt to correct/substitute
             if not r[0]: #if we have a null result for a gauge reading
                 #get readings 15 mins behind and ahead
                 self.cur.execute("SELECT value FROM hourlygaugereading " \
@@ -166,38 +166,64 @@ class DatasetBuilder:
                     values.append(offsetval)
                 else:
                     values = None
-                    return values #else this time point cannot be used to train with due to missing values
-            else: #else put the value in the list
+                    return values #else this time point cannot be used to train with due to missing values - return null list
+            else: #else put the value in the list, they are valid
                 values.append(r[0])
 
         return values
 
+
+    #Collect the future readings from the epoch. These are the actual values that we are predicting
     def ingest_future_measure_readings(self, start):
-        bottomtimewindow = start + datetime.timedelta(hours=6)
+        timetopredict = start + datetime.timedelta(hours=self.epoch -1)
         values = []
 
-        #Get the hourly readings going back 6 hours from timenow (not including timenow readings)
-        self.cur.execute("SELECT value, datetime FROM hourlygaugereading " \
+        #Retrieve the future reading from the database using the provided epoch
+        self.cur.execute("SELECT value FROM hourlygaugereading " \
                          "WHERE measureid = %s" \
-                            "AND extract('minutes' from datetime) = 0" \
-                            "AND datetime >= %s" \
-                            "AND datetime < %s" \
-                         "ORDER BY datetime ASC", (
-                         self.gaugemeasure, datetime.datetime.strftime(start, '%d-%m-%Y %H:%M'),
-                         datetime.datetime.strftime(bottomtimewindow, '%d-%m-%Y %H:%M'),))
-
+                         "AND extract('minutes' from datetime) = 0" \
+                         "AND datetime = %s", (
+                             self.gaugemeasure, datetime.datetime.strftime(timetopredict, '%d-%m-%Y %H:%M'),))
         result = self.cur.fetchall()
-        for r in result:
-            if not r[0]: #if we have a null result for a gauge reading
-                    values = None
-                    return values #else this time point cannot be used to train with due to missing values
-            else: #else put the value in the list
-                values.append(r[0])
 
-        return values
+        if not result[0][0]: #if we have a null result for the gauge reading at given epoch
+            values = None
+            return values
+        else:
+            values.append(result[0][0])
+            return values
 
-obj = DatasetBuilder('01-01-2017-0000','31-01-2017-2355','catchment1.csv','cat1_jan2017')
 
+obj = DatasetBuilder('01-01-2016-0000', '31-12-2016-2355', '2133-level-stage-i-15_min-mASD', 'cat1_2016_6hour.csv' ,6)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='argument handler')
+    parser.add_argument('-start', '-s', required= True, help='Timestamp to read in data from - format: dd-mm-yyyy-hhmm')
+    parser.add_argument('-end', '-e', required=True, help='Timestamp to stop reading in data - format: dd-mm-yyyy-hhmm')
+    parser.add_argument('-measure', '-m', required=True, help='Measure for which to build training set for')
+    parser.add_argument('-output', '-o', required=True, help='The name and path of the output CSV file')
+    parser.add_argument('-neuroncount', '-n', required=False, help='The number of neurons to use in the second hidden layer')
+    args = parser.parse_args()
+
+    try:
+        year = datetime.datetime.strptime(args.year, '%Y')
+    except ValueError:
+        print('The year was not entered in the correct format of yyyy')
+        exit()
+
+    try:
+        read_in_archive_data(year, args.path, args.station)
+    except FileNotFoundError:
+        print('No such file or directory')
+
+#This script should take the arguments:
+#start and stop datetime
+#measure
+#output file
+#output neuron? - only ever a single neuron will be the output and using one avoids missing values
+
+#option - number of neurons on second hidden layer
 
 
 
