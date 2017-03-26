c2525983c04261a76d3bc34e4785703e0f6f4da6
James M <jm824@kent.ac.uk>
2017-03-26 22:36:29 +0100

    Script now works as a proper command line script. A model, data and minmax file must be provided. Pridictions will then be made and a chart produced showing the results of the predicted vs actual.

--- Python Initialising Scripts/NeuralNet/read_model_plot.py
+++ Python Initialising Scripts/NeuralNet/read_model_plot.py
@@ -1,52 +1,103 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from pybrain.tools.customxml.networkreader import NetworkReader
 import csv
+import datetime as dt
+import os
+import matplotlib.ticker as mtick
 
 """
-This script that loads a trained neural net and data to activate (put through network).
-The prediction results are then plotted on a graph next to the actual recordings.
+This script loads a trained neural net and a data set to run through the neural net.
+This is for verification (evaluating a model).
+
+The data must be provided in the correct format (produced by the dataset_builder.py script).
+The data will be read in and a prediction made for each epoch. A optional minman file can be
+provided which will be used to de-normalize the neural net output so that the 'real' predictions
+are shown.
+
+A chart is then produced showing the predicted level vs the actual reading.
 """
 
-net = NetworkReader.readFrom('Trained_networks/cat1_2016_basic_9hour.csv_normalized.xml')
+def run_model(model, data, minmax = None):
+    #check the model provided exists
+    if not os.path.isfile(model):
+        exit('Model file not found.')
 
-with open('BASIC/verification/cat1_jan2017_basic_12hour_normalized.csv') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    x = []
-    y = []
-    x2 = []
-    y2 = []
-    count = 0
-    #load the data ready to be plotted on the chart
-    for row in csvreader:
-        count += 1
-        x.append(count)
-        y.append(row[-1:])
-        x2.append(count)
-        y2.append(net.activate(row[:-1]))
+    #check the data file exists
+    if not os.path.isfile(data):
+        exit('Data file not found.')
 
-    #read in the min max values used to normalize the data
-    with open('BASIC/cat1_2016_basic_12hour.csv_min_max.txt') as denorm:
-        minmax = []
-        for line in denorm:
-            lin = line.split(",")
-            minmax.append(float(lin[-1].strip("\n")))
+    #check the minmax file exists
+    if minmax:
+        if not os.path.isfile(minmax):
+            exit('minmax file not found.')
 
-    # go through both lists and denormalise for better visualisation
-    for plot in y:
-        plot[0] = float(plot[0]) * (minmax[0] - minmax[1]) + minmax[1]
-    for plot in y2:
-        plot[0] = float(plot[0]) * (minmax[0] - minmax[1]) + minmax[1]
+    #read the model into pybrain
+    net = NetworkReader.readFrom(model)
 
-    plt.figure(figsize=(10, 8))
-    plt.plot(x, y, label='Actual readings')
-    plt.plot(x2, y2, label='Predicted readings')
-    plt.xlabel('Epoch', size=25)
-    plt.ylabel('Water Level (meters)', size = 25)
-    plt.title('+12 hours January 2017 - predicted vs actual', size=30)
-    plt.tick_params(axis='x', labelsize=15)
-    plt.tick_params(axis='y', labelsize=15)
-    plt.rc('legend', fontsize=25)
+    #read in the csv data to run through the model
+    with open(data) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        x = []
+        y = []
+        x2 = []
+        y2 = []
+        count = 0
+        #load the data ready to be plotted on the chart
+        for row in csvreader:
+            #Work out what hour this prediction is for by reading metadata
+            if count == 0:
+                hour = row[-2:-1]
+            x.append(row[-1])
+            y.append(row[-3:-2])
+            y2.append(net.activate(row[:-3]))
+            count += 1
 
-    plt.legend()
-    plt.show()
+        x = [dt.datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in x]
+
+        #if a minmax file is provided then denormalize the data before plotting
+        if minmax:
+            #read in the min max values used to normalize the data
+            with open(minmax) as denorm:
+                minmax = []
+                for line in denorm:
+                    lin = line.split(",")
+                    minmax.append(float(lin[-1].strip("\n")))
+
+            # go through both lists and denormalise for better visualisation
+            for plot in y:
+                plot[0] = float(plot[0]) * (minmax[0] - minmax[1]) + minmax[1]
+            for plot in y2:
+                plot[0] = float(plot[0]) * (minmax[0] - minmax[1]) + minmax[1]
+
+        #Format chart and axis scales etc
+        plt.figure(figsize=(8, 10))
+        plt.plot(x, y, label='Actual readings')
+        plt.plot(x, y2, label='Predicted readings')
+        plt.xlabel('Date & Time')
+        plt.ylabel('Water Level (meters)')
+        plt.title('+' + hour[0] + 'hours: Predicted vs Actual')
+        plt.tick_params(axis='x')
+        plt.tick_params(axis='y')
+        plt.xticks(rotation='vertical')
+        plt.rc('legend')
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y %H:%M'))
+        #plt.gca().yaxis.set_major_formatter(mtick.FormatStrFormatter('%.3f'))
+        plt.gcf().autofmt_xdate()
+
+        #draw chart
+        plt.legend()
+        plt.show()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='argument handler')
+    parser.add_argument('model', help='A neural network (pybrain) model as a .xml file')
+    parser.add_argument('data_file', help='The .csv file containing the data to activate the model with')
+    parser.add_argument('--minmax', '-m', required = False, help='The .txt file containing the minmax values used to de-normalize the data')
+    args = parser.parse_args()
+
+    run_model(args.model, args.data_file, args.minmax)
 
